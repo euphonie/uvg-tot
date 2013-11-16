067889ba780e064ee55d3d1df4b47a9e22b42c1c
Joel Cantoral <cs.joel.09@gmail.com>
2013-11-16 12:25:34 -0600

    added imports, api creation and route /robots.txt

--- backend.py
+++ backend.py
@@ -1,7 +1,44 @@
 import os
 from flask import Flask
+from flask import request, render_template, make_response, jsonify
+
+from flask.ext import restful
+from flask.ext.pymongo import PyMongo
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+
+from flask.ext.restful import reqparse
+
+DEFAULT_REPRESENTATIONS = {'application/json': output_json}
 
 app = Flask(__name__)
+
+mongo = PyMongo(app, config_prefix="MONGOHQ_URL")
+
+
+@app.route('/robots.txt')
+def robots():
+  res = app.make_response('User-agent: *\nAllow: /')
+  res.mimetype = 'text/plain'
+  return res
+
+
+@app.route('/')
+def hello():
+	return 'Hello World!'
+
+
+## Start of UVG Tot API
+
+api = restful.Api(app)
+api.representations = DEFAULT_REPRESENTATIONS
+
+
+
+@app.route('/')
+def hello():
+	return 'Hello World!'
+
 
 @app.route('/')
 def hello():
