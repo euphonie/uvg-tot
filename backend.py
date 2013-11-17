2ff11fd54fdd7aa8d0b90c98b180baee7b9a99d8
Joel Cantoral <cs.joel.09@gmail.com>
2013-11-16 20:27:49 -0600

    working get one, get all and post

--- backend.py
+++ backend.py
@@ -1,13 +1,20 @@
 import os
 import ast
+import datetime
 from flask import Flask
 from flask import request, render_template, make_response, jsonify
+
+import logging
+from logging.handlers import RotatingFileHandler
 
 from flask.ext import restful
 from flask.ext.pymongo import PyMongo
 from flask.ext.pymongo import MongoClient
 from bson.json_util import dumps
 from bson.objectid import ObjectId
+from werkzeug.routing import BaseConverter, ValidationError
+from itsdangerous import base64_encode, base64_decode
+from bson.errors import InvalidId
 
 from flask.ext.restful import reqparse
 
@@ -35,16 +42,20 @@ def valid_tag_list(value,name):
 	return ast.literal_eval(value)
 
 DEFAULT_REPRESENTATIONS = {'application/json': output_json}
-DEBUG = False
-
 
 app = Flask(__name__)
-if not DEBUG:
+
+if not app.debug:
 	MONGO_URL = os.environ.get('MONGOHQ_URL')
 	client = MongoClient(MONGO_URL)
 else :
 	client = MongoClient()
 db = client['app19537990']
+
+@app.errorhandler(500)
+def internal_error(exception):
+	app.logger.exception(exception)
+	return render_template('500.html'), 500
 
 @app.route('/robots.txt')
 def robots():
@@ -68,8 +79,8 @@ search_parser.add_argument('title', type=valid_regex)
 search_parser.add_argument('content',type=valid_regex)
 
 insertion_parser = reqparse.RequestParser()
-insertion_parser.add_argument('title', type=valid_regex)
-insertion_parser.add_argument('content',type=valid_regex)
+insertion_parser.add_argument('title', type=str)
+insertion_parser.add_argument('content',type=str)
 insertion_parser.add_argument('tag_list',type=valid_tag_list)
 insertion_parser.add_argument('contact_name',type=str)
 insertion_parser.add_argument('contact_phone',type=str)
@@ -78,10 +89,11 @@ insertion_parser.add_argument('contact_mail',type=str)
 class ArticleAPI(restful.Resource):
 	''' Handle Individual article resource'''
 	def get(self, article_id=-1):
-		return db.articles.find_or_404({'_id':article_id})
-
+		app.logger.info(article_id)
+		return db.articles.find({'_id':ObjectId(article_id)})
+		
 	def post(self):
-		article = insert_parser.parse_args()
+		article = insertion_parser.parse_args()
 		article["creation_date"] = datetime.datetime.utcnow()
 		response = db.articles.insert(article)
 		return response
@@ -101,4 +113,12 @@ class ArticleAPIList(restful.Resource):
 
 
 api.add_resource(ArticleAPIList, '/articles')
-api.add_resource(ArticleAPI,'/article','/article/<ObjectId:article_id>')
+api.add_resource(ArticleAPI,'/article','/article/<article_id>')
+
+
+if __name__ == '__main__':
+	handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
+	handler.setLevel(logging.INFO)
+	app.logger.addHandler(handler)
+	app.run(debug=False)
+	
