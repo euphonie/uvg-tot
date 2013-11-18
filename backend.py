3065fe98e3b1a49340481295e6d8d8566176e18d
Joel Cantoral <cs.joel.09@gmail.com>
2013-11-17 23:19:22 -0600

    fixed search query param

--- backend.py
+++ backend.py
@@ -65,7 +65,15 @@ def robots():
 
 @app.route('/')
 def feed():
-	return render_template('feed.html',articles=db.articles.find())
+	query = request.args.get('q')
+	if query is not None:
+		clean_dict = {}
+		clean_dict['title'] = {'$regex':query,'$options':'i'}
+		clean_dict['content'] = {'$regex':query,'$options':'i'}
+		clean_dict['tag_list'] = {'$elemMatch':{'tag':query}}
+		return render_template('feed.html',articles=db.articles.find({'$or':[{'title':clean_dict['title']}, {'content':clean_dict['content']}, {'tag_list':clean_dict['tag_list']}]}),results=True)
+	else:
+		return render_template('feed.html',articles=db.articles.find())
 	
 @app.route('/about')
 def about():
@@ -95,17 +103,6 @@ def publish():
 	flash('Publicado exitosamente!','success')
 	db.articles.insert(values)
 	return render_template('feed.html',articles=db.articles.find())
-
-@app.route('/q/<query>')
-def q(query=''):
-	if query:
-		clean_dict = {}
-		clean_dict['title'] = {'$regex':query,'$options':'i'}
-		clean_dict['content'] = {'$regex':query,'$options':'i'}
-		clean_dict['tag_list'] = {'$elemMatch':{'tag':query}}
-		return render_template('feed.html',articles=db.articles.find({'$or':[{'title':clean_dict['title']}, {'content':clean_dict['content']}, {'tag_list':clean_dict['tag_list']}]}),results=True)
-	else:
-		return {},404
 	
 ## Start of UVG Tot API
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,3 @@
 $(document).ready(function(){
-    $('.submitQuery').removeClass('disabled');
-    $('.submitQuery').on('click',function(){
-        query = $(this).prev().find('.q').val();
-        $(this).closest('.search').attr('action',$(this).closest('.search').attr('action')+"/"+query);
-    });
     $("#tag-list").select2({tags:[]});
 });
